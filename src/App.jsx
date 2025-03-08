6f5d46c017b2580b1b740f1687c42e87bf591bb4
Leotucreador <leonardojosegutierrezpabon70404@gmail.com>
2025-03-07 23:08:58 -0500

    error 404 solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,29 @@ import { Notification } from "./Pages/Notification";
 import { Messages } from "./Pages/Messages";
 import { User } from "./edit/User";
 
-
-
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        {/* Ruta principal */}
+        <Route path="/" element={<Login />} />
 
-        <Route path="/Home" element={<Home/>}/>
-        <Route path="/Watch" element={<Watch/>} />
+        {/* Rutas de la aplicación */}
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Watch" element={<Watch />} />
         <Route path="/groups" element={<h1 className="text-center text-white">Grupos</h1>} />
         <Route path="/gaming" element={<h1 className="text-center text-white">Juegos</h1>} />
         <Route path="/menu" element={<h1 className="text-center text-white">Menú</h1>} />
-        <Route path="/messages" element={<Messages/>} />
-        <Route path="/Notis" element={<Notification/>}/>
-        <Route path="/Profiles" element={<Profiles/>} />
-        <Route path="register" element={<Register/>}/>
-        <Route path="User" element={<User/>}/>
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/Notis" element={<Notification />} />
+        <Route path="/Profiles" element={<Profiles />} />
+
+        {/* Rutas de autenticación y usuario */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/User" element={<User />} />
+
+        {/* Ruta para páginas no encontradas (404) */}
+        <Route path="*" element={<h1>Página no encontrada</h1>} />
       </Routes>
     </Router>
   );
