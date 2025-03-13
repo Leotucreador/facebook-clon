4b1a2e04c6b61cae26485f446c0f46520e74386b
Leotucreador <leonardojosegutierrezpabon70404@gmail.com>
2025-03-12 22:03:10 -0500

    deployment

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
+  plugins: [tailwindcss(), react()],
+  base: '/', 
   build: {
-    sourcemap: false,
     outDir: 'dist',
-    assetsDir: 'assets', // Asegura que los assets se coloquen en la carpeta correcta
-  },
-  server: {
-    headers: {
-      'Cache-Control': 'public, max-age=31536000, immutable'
-    }
+    assetsDir: 'assets',
+    sourcemap: false,
+    cssCodeSplit: false, 
   }
 })
